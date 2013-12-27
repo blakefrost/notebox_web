ade0dfefb6352b0ce5a326a63bbaa16787e8262b
Blake Taylor <blakefrost@gmail.com>
2013-12-27 02:39:58 -0500

    Render timers as a list

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,7 +1,20 @@
 class TimersController < ApplicationController
 
   def index
-    @timers = []
+    date = Date.today - params[:days_ago].to_i.days
+
+    @timers = notebox.fetch_entries(topic: 'timer', date: date)
+
+    # Why does slice throw an exception if the key is missing?
+    @options = {
+      days_ago: params[:days_ago],
+      topic: params[:topic]
+    }.delete_if { |k, v| v.nil? }
+
+    # Get dir listing of notes root ( This could work for following sub topics too)
+    @topics = Dir.entries(NOTES_ROOT) # Get topic listing
+    @topics.reject! { |t| t.match(/^\./) } # Remove hidden files
+
   end
 
   def show
@@ -11,6 +24,12 @@ class TimersController < ApplicationController
   end
 
   def update
+  end
+
+private
+
+  def notebox
+    @notebox ||= Notebox::Box.new(NOTES_ROOT)
   end
 
 end
