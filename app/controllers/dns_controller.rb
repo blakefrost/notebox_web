bb46e8f60507b63f7e1f546fefb34756b35d0d9d
Blake Taylor <blakefrost@gmail.com>
2014-02-12 16:22:13 -0500

    Render and integrate the host file

--- app/controllers/dns_controller.rb
+++ app/controllers/dns_controller.rb
@@ -2,6 +2,8 @@ require_dependency 'notebox/box'
 
 class DnsController < ApplicationController
 
+  HOSTS_FILE = File.join(Rails.root, "tmp", "hosts")
+
   def index
     @running = `ps aux | grep dnsproxy | grep -v grep`.present?
     @host_configuration = HostConfiguration.singleton
@@ -21,18 +23,15 @@ class DnsController < ApplicationController
   end
 
   def hosts
-    @host_configuration = HostConfiguration.singleton
-    @blocked_list = BlockedDomain.all.order_by(:value.asc)
-    @text = @host_configuration.value
-    @text << "\n\n# Blocked Domains\n" << @blocked_list.select(&:blocked).map { |bd| "127.0.0.1 *.#{bd.value}" }.join("\n")
-
-    render text: @text, content_type: :text
+    render_host_file
+    render text: File.read(HOSTS_FILE), content_type: :text
   end
 
 private
 
   def start_dnsproxy(password)
-    `  bash -c "echo -n \"#{password}\" | sudo -S dnsproxy.py -s 8.8.8.8 </dev/null &>/dev/null &"`
+    render_host_file
+    `  bash -c "echo -n \"#{password}\" | sudo -S dnsproxy.py -s 8.8.8.8 -f "#{HOSTS_FILE}" -C </dev/null &>/dev/null &"`
   end
 
   def stop_dnsproxy(password)
@@ -43,6 +42,15 @@ private
   def restart_dnsproxy(password)
     stop_dnsproxy(password)
     start_dnsproxy(password)
+  end
+
+
+  def render_host_file
+    @host_configuration = HostConfiguration.singleton
+    @blocked_list = BlockedDomain.all.order_by(:value.asc)
+    @text = @host_configuration.value
+    @text << "\n\n# Blocked Domains\n" << @blocked_list.select(&:blocked).map { |bd| "127.0.0.1  #{bd.value}\n127.0.0.1  *.#{bd.value}" }.join("\n")
+    File.write(HOSTS_FILE, @text)
   end
 
 end
